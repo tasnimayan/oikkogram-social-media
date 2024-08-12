8f2cfb19e247ceec5fc1033b01f889a88beb97ad
Tasnim Chowdhury <tasnimayan22@gmail.com>
2024-08-12 11:53:26 +0600

    email solve

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -10,7 +10,6 @@ const authOptions: NextAuthOptions = {
       server: {
         host: process.env.EMAIL_HOST,
         port: 465,
-        secure:true,
         auth: {
           user: process.env.EMAIL_AUTH_USER,
           pass: process.env.EMAIL_AUTH_PASS,
